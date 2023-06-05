27ed1eeb3e70289d7eaf89d2a68d4aaed441d826
Patrycja Dziabala <patrycja.dziabala@gmail.com>
2023-06-05 11:41:47 +0200

    Initial Commit

---
+++ PedroPascalLand/PedroPascalLandApp.swift
@@ -0,0 +1,17 @@
+//
+//  PedroPascalLandApp.swift
+//  PedroPascalLand
+//
+//  Created by Patka on 05/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct PedroPascalLandApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
